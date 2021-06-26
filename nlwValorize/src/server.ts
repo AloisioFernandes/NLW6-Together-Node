421591b870076fa3423e09cba5631502b06cbe5e
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-06-25 21:02:14 -0300

    Configurando banco de dados.

--- nlwValorize/src/server.ts
+++ nlwValorize/src/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata' // necessário para notations do typeorm
 import express from 'express'
 
+import './database' // create connection
+
 const app = express()
 
 app.listen(3000, () => console.log('Server is running'))
