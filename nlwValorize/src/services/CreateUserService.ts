dbc376bcb3b18f305fe65d207d1e693961a2bd9f
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-06-29 19:09:11 -0300

    Service Create User feito.

--- nlwValorize/src/services/CreateUserService.ts
+++ nlwValorize/src/services/CreateUserService.ts
@@ -1,3 +1,4 @@
+import { UsersRepositories } from "../repositories/UsersRepositories"
 
 interface IUserRequest {
   name: string;
@@ -7,10 +8,30 @@ interface IUserRequest {
 
 class CreateUserService {
   async execute({ name, email, admin }: IUserRequest) {
-    //
+    const usersRepository = new UsersRepositories()
+
+    if(!email) {
+      throw new Error("Email incorrect")
+    }
+
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    })
+
+    if(userAlreadyExists) {
+      throw new Error("User already exists")
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin
+    })
+
+    await usersRepository.save(user)
+
+    return user
   }
 }
 
 export { CreateUserService }
-
-// 1:21:00
