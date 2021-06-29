a213f7461209d216df17c6b8bf28b240f7c0a385
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-06-29 19:29:25 -0300

    Rota para adicionar usuário.

--- nlwValorize/src/server.ts
+++ nlwValorize/src/server.ts
@@ -1,8 +1,14 @@
 import 'reflect-metadata' // necessário para notations do typeorm
 import express from 'express'
 
+import { router } from './routes'
+
 import './database' // create connection
 
 const app = express()
+
+app.use(express.json())
+
+app.use(router)
 
 app.listen(3000, () => console.log('Server is running'))
